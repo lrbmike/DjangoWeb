8e03bbbf49b55a917fc6845bbf9484f113a0200d
lrbmike <lrbmike@163.com>
2014-07-17 14:31:34 +0800

    update db config

--- pythonWeb/models.py
+++ pythonWeb/models.py
@@ -4,7 +4,7 @@ import datetime
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=50)
-    age = models.IntegerField(max_length=2)
+    age = models.IntegerField(null=True)
     createTime = models.DateTimeField(default=datetime.datetime.now())
 
     def __str__(self):
@@ -21,5 +21,18 @@ class Teacher(models.Model):
     def __str__(self):
         if self.name:
             return "%d, %s, %s" % (self.id, self.name, str(self.createTime))
+        else:
+            return ""
+
+
+class Grade(models.Model):
+    name = models.CharField(max_length=50)
+    teacherId = models.IntegerField(null=True)
+    userId = models.IntegerField(null=True)
+    createTime = models.DateTimeField(default=datetime.datetime.now())
+
+    def __str__(self):
+        if self.name:
+            return "%d, %s,, %d, %d, %s" % (self.id, self.name, self.teecherId, self.userId, str(self.createTime))
         else:
             return ""
