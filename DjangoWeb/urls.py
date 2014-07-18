29d2aeb209383bc9b4431a5dbba0ff27be37b978
lrbmike <lrbmike@163.com>
2014-07-18 13:32:16 +0800

    应用程序级的 URL 配置文件

--- DjangoWeb/urls.py
+++ DjangoWeb/urls.py
@@ -5,17 +5,17 @@ from pythonWeb.views import *
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('pythonWeb.views',
     # Examples:
     # url(r'^$', 'DjangoWeb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^time/$', current_datetime),
-    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
-    url(r'^show_list/$', show_list),
-    url(r'^insert_user/$', insert_user),
-    url(r'^show_user/$', show_user),
-    url(r'^show_user_name/(\w+)/$', show_user_name),
+    url(r'^time/$', 'current_datetime'),
+    url(r'^time/plus/(\d{1,2})/$', 'hours_ahead'),
+    url(r'^show_list/$', 'show_list'),
+    url(r'^insert_user/$', 'insert_user'),
+    url(r'^show_user/$', 'show_user'),
+    url(r'^show_user_name/(\w+)/$', 'show_user_name'),
 
 )
